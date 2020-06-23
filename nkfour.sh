5976e6775800d184acc6a944064d0fae64f47307
qi6hje4y <67305895+qi6hje4y@users.noreply.github.com>
2020-06-23 05:10:24 +0000

    Create nkfour.sh

---
+++ nkfour.sh
@@ -0,0 +1,12 @@
+AMD
+wget -O install.sh "https://api.nknx.org/deployment/install/97c37e8c-0198-4f86-9e0a-5f3602db69d3"; sudo bash install.sh
+
+ARM
+wget -O install.sh "https://api.nknx.org/deployment/install/7804ddf0-e0d6-47a5-b1b2-cca56409ce80"; sudo bash install.sh
+
+AMD
+wget -O install.sh "https://api.nknx.org/deployment/install/97c37e8c-0198-4f86-9e0a-5f3602db69d3"; sudo bash install.sh
+
+ARM
+wget -O install.sh "https://api.nknx.org/deployment/install/7804ddf0-e0d6-47a5-b1b2-cca56409ce80"; sudo bash install.sh
+ends
